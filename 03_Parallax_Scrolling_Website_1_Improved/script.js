993d2a4a4a4365ed7e42fe84f0b0a00d8ce25fc9
Aiden <aidenlh@qq.com>
2024-02-29 11:50:58 +0800

    fix: 进一步修复缩放不正确的问题

--- 03_Parallax_Scrolling_Website_1_Improved/script.js
+++ 03_Parallax_Scrolling_Website_1_Improved/script.js
@@ -4,15 +4,24 @@ let treeRight = document.getElementById('tree-right');
 let gateLeft = document.getElementById('gate-left');
 let gateRight = document.getElementById('gate-right');
 
+document.getElementById('parallax').style.height = document.getElementById('gate-right').clientHeight - 1 + 'px';
+window.addEventListener('resize', () => {
+    document.getElementById('parallax').style.height = document.getElementById('gate-right').clientHeight - 1 + 'px';
+})
+
 window.addEventListener('scroll', () => {
     let value = window.scrollY;
 
     text.style.marginTop = value * 2.5 + 'px';
     treeLeft.style.left = value * -1.5 + 'px';
     treeRight.style.left = value * 1.5 + 'px';
-    gateLeft.style.left = value * 0.5 + 'px';
-    gateRight.style.left = value * -0.5 + 'px';
-    // 用代码实现fix的效果
-    gateLeft.style.top = value + 'px';
-    gateRight.style.top = value + 'px';
+
+    if (value<520){
+        // 用代码实现fix的效果
+        gateLeft.style.top = value + 'px';
+        gateRight.style.top = value + 'px';
+
+        gateLeft.style.left = value * 0.5 + 'px';
+        gateRight.style.left = value * -0.5 + 'px';
+    }
 })
